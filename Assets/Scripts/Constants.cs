856b5b9f21742e921737fb1080c02cf7f66ba72e
Keir Smith <kaffse@gmail.com>
2019-09-08 13:12:04 +0100

    Added basic student needs

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -7,5 +7,8 @@ namespace MageAcademy
     public static class Constants
     {
         public static float STUDENT_LEARN_RATE = 0.001f;
+        public static float STUDENT_HUNGER_RATE = 0.005f;
+        public static float STUDENT_BOREDOM_RATE = 0.001f;
+        public static float STUDENT_SLEEPINESS_RATE = 0.005f;
     }
 }
--- Assets/Scripts/Students/StudentNeeds.cs
+++ Assets/Scripts/Students/StudentNeeds.cs
@@ -1,9 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MageAcademy;
 
 public class StudentNeeds : MonoBehaviour
 {
     public float studentHunger = 1f;
     public float studentBoredom = 0f;
+    public float studentSleepiness = 0f;
+
+    private GameTime gameTime;
+
+    private void Awake()
+    {
+        gameTime = TimeManager.Instance.currentTime;
+    }
+
+    void OnEnable()
+    {
+        gameTime.OnMinuteIncrement += OnMinuteChange;
+    }
+
+    void OnDisable()
+    {
+        gameTime.OnMinuteIncrement -= OnMinuteChange;
+    }
+
+    private void OnMinuteChange()
+    {
+        studentHunger = Mathf.Clamp(studentHunger - Constants.STUDENT_HUNGER_RATE, 0f, 1f);
+        studentBoredom = Mathf.Clamp(studentBoredom + Constants.STUDENT_BOREDOM_RATE, 0f, 1f);
+        studentSleepiness = Mathf.Clamp(studentSleepiness + Constants.STUDENT_SLEEPINESS_RATE, 0f, 1f);
+    }
 }
